a544d604c574a81c7e135b380c519bf2a8e2c878
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-08 11:17:52 -0300

    Removendo o menu do main e deixando apenas as instâncias, limpando o código

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,7 @@
-import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        int opcao;
-        double valor;
-
-        Scanner leia = new Scanner(System.in);
-
-        ContaBancaria.validacao();
-        ContaBancaria conta = new ContaBancaria();
-
-        do {
-            System.out.println("MENU");
-            System.out.println("1 - Ver Saldo");
-            System.out.println("2 - Depositar");
-            System.out.println("3 - Sacar");
-            System.out.println("0 - Sair");
-            opcao = leia.nextInt();
-
-            switch(opcao){
-
-                case 1:
-                   System.out.println("Seu saldo é de :R$ " + conta.getSaldo());
-                    break;
-
-                case 2:
-                    System.out.println("Quanto você deseja depositar: ");
-                    valor = leia.nextDouble();
-                    conta.depositar(valor);
-                    break;
-
-                case 3:
-                    System.out.println("Quanto você deseja sacar: ");
-                    valor = leia.nextDouble();
-                    conta.sacar(valor);
-                    break;
-
-                case 0:
-                    System.out.println("Finalizando Atendimento...");
-                    break;
-
-                default:
-                    System.out.println("Opção Inválida!");
-                    break;
-            }
-        }while(opcao != 0);
+    public static void main(String[] args) {
+       ContaBancaria conta = ContaBancaria.criarConta();
+       conta.menu();
     }
 }
