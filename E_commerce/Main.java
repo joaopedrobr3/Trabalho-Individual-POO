0ec81ea28291e615d82fe6029f28d927f251681b
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-13 16:31:43 -0300

    Instânciando o cliente, o pedido e o método fecharPedido(). Criando os clientes e os pedidos.

--- E_commerce/Main.java
+++ E_commerce/Main.java
@@ -3,5 +3,18 @@ package E_commerce;
 public class Main {
     public static void main(String[] args) {
 
+        Cliente c1 = new Cliente (1, "João Pedro", "151.865.247-60");
+        Pedido p1 = new Pedido(1 , c1);
+        p1.adicionarItem(new ItemPedido("Mouse Gamer",200.00, 1));
+        p1.adicionarItem(new ItemPedido("Teclado Gamer",300.00, 1));
+        p1.adicionarItem(new ItemPedido("Monitor",2000.00, 1));
+        p1.fecharPedido();
+
+        Cliente c2 = new Cliente (2, "Izabella", "687.364.230-70");
+        Pedido p2 = new Pedido(2 , c2);
+        p2.adicionarItem(new ItemPedido("Batom",80.00, 2));
+        p2.adicionarItem(new ItemPedido("Vestido",300.00, 1));
+        p2.adicionarItem(new ItemPedido("Pulseira",400.00, 1));
+        p2.fecharPedido();
     }
 }
