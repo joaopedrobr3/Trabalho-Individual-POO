56e5df26b9ce2d17a6867daaa91a61536ba87528
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-13 16:32:08 -0300

    Criando o método fecharPedido().

--- E_commerce/Pedido.java
+++ E_commerce/Pedido.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pedido {
+    private Integer id;
     private Cliente cliente;
     private List<ItemPedido> itens;
 
 
-    public Pedido(Cliente cliente, List<ItemPedido> itens) {
+    public Pedido(Integer id, Cliente cliente) {
+        this.id = id;
         this.cliente = cliente;
-        this.itens = new ArrayList<>();
+        this.itens = new ArrayList<>() ;
+
     }
+
     public void adicionarItem(ItemPedido item){
         itens.add(item);
     }
+
     public Double calcularTotal(){
         Double total = 0.0;
 
@@ -23,14 +28,52 @@ public class Pedido {
         }
         return total;
     }
-    public Double calcularFrete(){
-        if(total > 250){
+
+    public Double calcularFrete() {
+        if (calcularTotal() >= 250) {
             return 0.0;
-        }
-        else{
+        } else {
             return 25.0;
         }
     }
 
+    public void fecharPedido(){
+            double total = calcularTotal();
+            double frete = calcularFrete();
+            double totalFinal = total + frete;
+
+
+
+            System.out.println("==================");
+            System.out.println(" Recibo do pedido Número: " + getId());
+            System.out.println("\nCliente: " + cliente.getNome());
+            System.out.println("CPF: " + cliente.getCpf());
+            System.out.println("==================\n");
+              for (ItemPedido item : itens) {  //
+               System.out.println(item);
+             }
+            System.out.println("\nTotal: R$" + calcularTotal());
+            if(frete == 0){
+                System.out.println("Frete: Grátis!\n");
+            }
+            else{
+                System.out.println("Frete: R$" + calcularFrete());
+            }
+
+
+       }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
 
 }
