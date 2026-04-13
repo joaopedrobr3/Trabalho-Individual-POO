10fff8ad3d8f6f3d852ea5c106f799bba1c1e7ac
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-13 16:29:48 -0300

    Trocando as posições dos atributos

--- E_commerce/Cliente.java
+++ E_commerce/Cliente.java
@@ -1,24 +1,28 @@
 package E_commerce;
 
 public class Cliente {
+    private Integer id;
     private String nome;
     private String cpf;
 
-    public Cliente(String nome, String cpf) {
+
+    public Cliente(Integer id,String nome, String cpf) {
+        this.id = id;
         this.nome = nome;
         this.cpf = cpf;
+
     }
 
     public String getNome() {
         return nome;
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getCpf() {
         return cpf;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
 
 }
