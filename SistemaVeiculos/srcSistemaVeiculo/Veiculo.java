e7100b00f8f8a5a805ab6e3b8d889c771190f967
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-15 14:18:55 -0300

    Adicionando o atributo Id e contador na classe veículo (por questões de estetica na formatação final)

--- SistemaVeiculos/srcSistemaVeiculo/Veiculo.java
+++ SistemaVeiculos/srcSistemaVeiculo/Veiculo.java
@@ -1,18 +1,50 @@
+import Interfaces.Fretavel;
+import Interfaces.Tributavel;
+
 import java.time.LocalDate;
 
-public abstract sealed class Veiculo implements Tributavel, Fretavel permits  {
-    final String placa;
-    final String marca;
+public abstract sealed class Veiculo implements Tributavel, Fretavel permits Caminhao, CarroPasseio {
+    public static int contador = 1;
+    private int id;
+    private final String placa;
+    private final String marca;
     private double valorLocacaoDiaria;
     public final LocalDate anoFabricacao;
     private double precoFipe;
 
-    public Veiculo(String placa, String marca, double valorLocacaoDiaria, LocalDate anoFabricacao, double precoFipe) {
+
+    public Veiculo(int id,String placa, String marca, double valorLocacaoDiaria, LocalDate anoFabricacao, double precoFipe){
+
+        if (placa == null || placa.isBlank())
+            throw new IllegalArgumentException("Placa inválida");
+
+        if (marca == null || marca.isBlank())
+            throw new IllegalArgumentException("Marca inválida");
+
+        if (valorLocacaoDiaria <= 0)
+            throw new IllegalArgumentException("Valor da diária inválido");
+
+        if (precoFipe <= 0)
+            throw new IllegalArgumentException("Preço FIPE inválido");
+
         this.placa = placa;
         this.marca = marca;
         this.valorLocacaoDiaria = valorLocacaoDiaria;
         this.anoFabricacao = anoFabricacao;
         this.precoFipe = precoFipe;
+        this.id = contador++;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public LocalDate getAnoFabricacao() {
+        return anoFabricacao;
     }
 
     public double getValorLocacaoDiaria() {
@@ -29,5 +61,12 @@ public abstract sealed class Veiculo implements Tributavel, Fretavel permits  {
 
     public void setPrecoFipe(double precoFipe) {
         this.precoFipe = precoFipe;
+    }
+    public int getIdadeVeiculo() {
+        return LocalDate.now().getYear() - anoFabricacao.getYear();
+    }
+
+    public int getId() {
+        return id;
     }
 }
